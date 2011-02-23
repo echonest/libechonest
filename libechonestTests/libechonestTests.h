bc8b093d085a99e2ae09bb6c32c0d5b12a490c09
Art Gillespie <art@tapsquare.com>
2011-02-23 15:40:11 -0700

    Initial Commit

---
+++ libechonestTests/libechonestTests.h
@@ -0,0 +1,17 @@
+//
+//  libechonestTests.h
+//  libechonestTests
+//
+//  Created by Art Gillespie on 2/23/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface libechonestTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ libechonestTests/libechonestTests.m
@@ -0,0 +1,33 @@
+//
+//  libechonestTests.m
+//  libechonestTests
+//
+//  Created by Art Gillespie on 2/23/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "libechonestTests.h"
+
+
+@implementation libechonestTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in libechonestTests");
+}
+
+@end
